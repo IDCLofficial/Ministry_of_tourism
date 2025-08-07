e691dfe3087247b0bc4b118bf2187715d853582e
jephthahdominic <stdominicjephthah63@gmail.com>
2025-08-07 13:33:05 +0200

    Worked on some corrections

--- src/app/events/eventsList.ts
+++ src/app/events/eventsList.ts
@@ -5,7 +5,7 @@ const events = [
     location: "Owerri, Imo State",
     title: "World Heritage Day: Imo Cultural Festival",
     description: "Celebrate World Heritage Day with a vibrant showcase of Imo State's cultural heritage, including traditional music, dance, art exhibitions, and local cuisine. The event aims to promote cultural preservation and tourism in the region.",
-    img: "/images/homeImage2.jpg",
+    img: "/images/culture.jpg",
     time: "9:00 am",
     organizer: "Imo State Ministry of Tourism, Creative Arts and Culture",
     phone: "0803 123 4567",
